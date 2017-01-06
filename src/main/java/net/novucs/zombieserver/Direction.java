8c9debb255e46d14d0223c34758e678a89ec6737
novucs <contact@novucs.net>
2017-01-06 13:46:50 +0000

    Add direction enum

---
+++ src/main/java/net/novucs/zombieserver/Direction.java
@@ -0,0 +1,34 @@
+package net.novucs.zombieserver;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public enum Direction {
+
+    NORTH("N"),
+    NORTH_WEST("NW"),
+    WEST("W"),
+    SOUTH_WEST("SW"),
+    SOUTH("S"),
+    SOUTH_EAST("SE"),
+    EAST("E"),
+    NORTH_EAST("NE");
+
+    private static final Map<String, Direction> BY_SHORTHAND = new HashMap<>();
+    private final String shorthand;
+
+    Direction(String shorthand) {
+        this.shorthand = shorthand;
+    }
+
+    public static Optional<Direction> getByShorthand(String shorthand) {
+        return Optional.ofNullable(BY_SHORTHAND.get(shorthand));
+    }
+
+    static {
+        for (Direction direction : values()) {
+            BY_SHORTHAND.put(direction.shorthand, direction);
+        }
+    }
+}
