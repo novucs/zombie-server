904e6182dbe6e9239231acde707d86baa949710a
novucs <contact@novucs.net>
2017-01-06 12:47:07 +0000

    Shift main into a runnable and give correct error message

--- src/main/java/net/novucs/zombieserver/Main.java
+++ src/main/java/net/novucs/zombieserver/Main.java
@@ -7,38 +7,34 @@ import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Main {
+public class Main implements Runnable {
 
     private static final Logger logger = Logger.getLogger(Main.class.getName());
-
-    private final InetAddress address;
-    private final int port;
-
-    public Main(InetAddress address, int port) {
-        this.address = address;
-        this.port = port;
-    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        new Main().run();
+    }
 
+    @Override
+    public void run() {
         // use a try/catch block to handle the case when opening
         // a socket fails...
         InetAddress address;
 
         try {
             // TODO you need to define classes to represent the world
-            // then here you should use WorldLoader to load the world and 
-            // convert it to your representation of the world to play 
-            // the game with your version of ZombieBot.
+            // then here you should use WorldLoader to load the world and
+            // convert it to your representation of the world to play
+            // the game with your version of ZombieBotImpl.
 
             // create an instance of our server to communicate with the
             // web frontend.
             address = InetAddress.getLocalHost();
         } catch (UnknownHostException ex) {
-            logger.log(Level.SEVERE, "Failed to connect to the client", ex);
+            logger.log(Level.SEVERE, "Localhost could not be resolved to an address", ex);
             return;
         }
 
@@ -46,6 +42,6 @@ public class Main {
         // get host address, rather than using 127.0.0.1, as this
         // will then be displayed when server waits for connection
         // which allows the address to then be typed into client.
-        ZombieServer server = new ZombieServer(address.getHostAddress(), 8085, ZombieBot.create());
+        new ZombieServer(address.getHostAddress(), 8085, ZombieBotImpl.create());
     }
 }
